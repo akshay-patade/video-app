c640139be166fb38f5c835b979035dc10d141e68
Akshay Patade <akshaypatade@gmail.com>
2023-12-04 19:06:21 -0500

    Created an empty backend foler

---
+++ front-end/src/app/login/verify-code.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VerifyCodeService {
+
+  //Verification code used to verify the user
+  private veriCode = "";
+  private pattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+  constructor() { }
+
+  setVerificationCode(): void{
+
+    let code: string = "";
+    for(let i = 1; i <= 6; i++) {
+
+      let randomNumber = Math.floor(Math.random() * this.pattern.length);
+      code += this.pattern[randomNumber];
+      
+      this.veriCode = code;
+    }
+  }
+
+  getVerificationCode(): string{
+
+    return this.veriCode;
+  }
+}
